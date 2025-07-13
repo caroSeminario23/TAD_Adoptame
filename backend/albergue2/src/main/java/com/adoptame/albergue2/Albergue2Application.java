5f52a972c5da936fad1176801492f2b4002fc2e3
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2025-07-13 16:24:32 -0500

    Albergue2 | Estructura del proyecto

---
+++ backend/albergue2/src/main/java/com/adoptame/albergue2/Albergue2Application.java
@@ -0,0 +1,13 @@
+package com.adoptame.albergue2;
+
+/**
+ * Hello world!
+ *
+ */
+public class Albergue2Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ backend/albergue2/src/main/java/com/adoptame/albergue2/model/TipoMascota.java
@@ -0,0 +1,42 @@
+package com.adoptame.albergue2.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tipo_mascota")
+public class TipoMascota {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tipo_mascota", nullable = false)
+    private Integer idTipoMascota;
+
+    @Column(name = "nombre", nullable = false, unique = true, length = 20)
+    private String nombre;
+
+    // Constructor vacío (necesario para JPA)
+    public TipoMascota() {
+    }
+
+    // Constructor con parámetros
+    public TipoMascota(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getters y setters
+    public Integer getIdTipoMascota() {
+        return idTipoMascota;
+    }
+
+    public void setIdTipoMascota(Integer idTipoMascota) {
+        this.idTipoMascota = idTipoMascota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
